4f3567691894931372040c5660e5f0cfdc3169d1
FaunuX <100310972+FaunuX@users.noreply.github.com>
2023-07-08 08:45:45 -0700

    make opposite swappers

--- Main.gd
+++ Main.gd
@@ -15,10 +15,10 @@ func _collect_coin(body, node):
 	coins += 1
 	$Coinsound.play()
 	$CanvasLayer/GUI/Label.text = str(coins)
-	$Level.call_deferred("remove_child", node.get_parent())
+	$Level.call_deferred("remove_child", node.get_parent().get_parent())
 
 
 
 func _on_level_child_entered_tree(node):
-	for child in node.find_children("Scene1"):
+	for child in node.find_child("*").find_children("Scene1"):
 		child.connect("body_entered", self._collect_coin.bind(child))
